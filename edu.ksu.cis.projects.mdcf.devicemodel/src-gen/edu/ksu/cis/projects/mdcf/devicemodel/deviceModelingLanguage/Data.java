32b8ba800cce1badb264abb4681222b8c4c24dbe
Robby <robby@k-state.edu>
2013-03-29 11:47:54 -0500

    Added data.

---
+++ edu.ksu.cis.projects.mdcf.devicemodel/src-gen/edu/ksu/cis/projects/mdcf/devicemodel/deviceModelingLanguage/Data.java
@@ -0,0 +1,18 @@
+/**
+ */
+package edu.ksu.cis.projects.mdcf.devicemodel.deviceModelingLanguage;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Data</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see edu.ksu.cis.projects.mdcf.devicemodel.deviceModelingLanguage.DeviceModelingLanguagePackage#getData()
+ * @model
+ * @generated
+ */
+public interface Data extends ComponentDecl
+{
+} // Data
---
+++ edu.ksu.cis.projects.mdcf.devicemodel/src-gen/edu/ksu/cis/projects/mdcf/devicemodel/deviceModelingLanguage/impl/DataImpl.java
@@ -0,0 +1,42 @@
+/**
+ */
+package edu.ksu.cis.projects.mdcf.devicemodel.deviceModelingLanguage.impl;
+
+import edu.ksu.cis.projects.mdcf.devicemodel.deviceModelingLanguage.Data;
+import edu.ksu.cis.projects.mdcf.devicemodel.deviceModelingLanguage.DeviceModelingLanguagePackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Data</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class DataImpl extends ComponentDeclImpl implements Data
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected DataImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return DeviceModelingLanguagePackage.Literals.DATA;
+  }
+
+} //DataImpl
