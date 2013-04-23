76af76606e510dfaa855e822638ea7de7411394b
Yu Jin Kim <yujin@ksu.edu>
2013-04-23 11:02:15 -0500

    Wellformedness check for basic type

---
+++ mdcf-dml-ast-wellformedness-test/src/test/java/edu/ksu/cis/santos/mdcf/dml/checker/test/WellformednessTest.java
@@ -0,0 +1,73 @@
+package edu.ksu.cis.santos.mdcf.dml.checker.test;
+/*
+Copyright (c) 2013 Robby, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+import static edu.ksu.cis.santos.mdcf.dml.serialization.XStreamer.toXml;
+import org.junit.Test;
+
+import edu.ksu.cis.santos.mdcf.dml.ast.*;
+import static edu.ksu.cis.santos.mdcf.dml.ast.Ast.Weak.*;
+import edu.ksu.cis.santos.mdcf.dml.checker.BasicTypeWellFormednessChecker;
+import edu.ksu.cis.santos.mdcf.dms.ModelExtractor;
+
+public class WellformednessTest {
+
+  @Test
+  public void testBasicTypeWellformedness() throws Exception {
+    final Model m = loadModel("dms.test", "edu.ksu.cis.santos.mdcf.dms.example");
+    BasicTypeWellFormednessChecker checker = new BasicTypeWellFormednessChecker(m);
+    checker.init();
+    
+    if(!checker.checkBasicTypeWellformedness_NonBasicType()){
+      throw new Exception(); 
+    }
+    if(!checker.checkBasicTypeWellformedness_CyclicInheritance()){
+      throw new Exception(); 
+    }
+  }
+  
+  @Test
+  public void testBasicTypeWellformednessNonBasic() throws Exception {
+    Model m = model(list(basicType("A", list(namedType("B")))));
+    String mXml = toXml(m);
+    System.out.println(mXml);
+
+    BasicTypeWellFormednessChecker checker = new BasicTypeWellFormednessChecker(m);
+    checker.init();
+    
+    if(checker.checkBasicTypeWellformedness_NonBasicType()){
+      throw new Exception(); 
+    }
+    if(!checker.checkBasicTypeWellformedness_CyclicInheritance()){
+      throw new Exception(); 
+    }
+  }
+  
+  @Test
+  public void testBasicTypeWellformednessCyclic() throws Exception {
+    Model m = model(list(basicType("A", list(namedType("B"))), basicType("B", list(namedType("A")))));
+    String mXml = toXml(m);
+    System.out.println(mXml);
+
+    BasicTypeWellFormednessChecker checker = new BasicTypeWellFormednessChecker(m);
+    checker.init();
+    
+    if(!checker.checkBasicTypeWellformedness_NonBasicType()){
+      throw new Exception(); 
+    }
+    if(checker.checkBasicTypeWellformedness_CyclicInheritance()){
+      throw new Exception(); 
+    }
+  }
+
+  Model loadModel(final String name, final String... packageNames) throws Exception {
+    final Model m = ModelExtractor.extractModel(packageNames);
+    return m;
+  }
+  
+}
---
+++ mdcf-dml-ast-wellformedness/src/main/java/edu/ksu/cis/santos/mdcf/dml/checker/BasicTypeWellFormednessChecker.java
@@ -0,0 +1,130 @@
+/**
+ * 
+ */
+package edu.ksu.cis.santos.mdcf.dml.checker;
+
+import edu.ksu.cis.santos.mdcf.dml.ast.AbstractVisitor;
+import edu.ksu.cis.santos.mdcf.dml.ast.BasicType;
+import edu.ksu.cis.santos.mdcf.dml.ast.Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author yjkim
+ *
+ */
+public class BasicTypeWellFormednessChecker{
+  class visitor extends AbstractVisitor {
+    public ArrayList<String> basicTypeList;
+    public HashMap<String, ArrayList<String>> inheritanceRelations;
+    
+    public visitor(){
+      this.basicTypeList = new ArrayList<String>();
+      this.inheritanceRelations = new HashMap<String, ArrayList<String>>();
+    }
+    
+    @Override
+    public boolean visitBasicType(BasicType node) {    
+      this.basicTypeList.add(node.name);
+   
+      if(!node.supers.isEmpty()){
+        
+        ArrayList<String> superList;
+        if((superList = this.inheritanceRelations.get(node.name)) == null){
+          superList = new ArrayList<String>();
+        }
+        
+        for(edu.ksu.cis.santos.mdcf.dml.ast.NamedType type : node.supers){
+          superList.add(type.name);
+        }
+        
+        this.inheritanceRelations.put(node.name, superList);
+      }
+      return true;
+    }
+  }
+
+  private Model m;
+  private visitor v;
+  
+  private ArrayList<String> basicTypeList;
+  private HashMap<String, ArrayList<String>> inheritanceRelations;
+  private HashMap<String, ArrayList<String>> completeInheritanceRelations;
+  
+  
+  public BasicTypeWellFormednessChecker(Model m){
+    this.m = m;
+  }
+  
+  public void init(){
+    v = new visitor();
+    v.visit(m);
+    
+    System.out.println("BasicType List: " + v.basicTypeList);
+    System.out.println("Inheritance Relation List: " + v.inheritanceRelations);
+    
+    this.basicTypeList = v.basicTypeList;
+    this.inheritanceRelations = v.inheritanceRelations;
+    this.completeInheritanceRelations = new HashMap<String, ArrayList<String>>();
+    
+    //Build the inheritance hierarchy for each
+    for(String key: this.inheritanceRelations.keySet()){
+      ArrayList<String> fullList = new ArrayList<String>();
+      buildInheritanceHierarchy(key, key, fullList);
+      this.completeInheritanceRelations.put(key, fullList);
+    }
+    
+    System.out.println("Complete Inheritance Relation List: " + this.completeInheritanceRelations);
+    
+  }
+
+  public boolean checkBasicTypeWellformedness_NonBasicType(){
+    //For each basic type, each of its super types is either another basic type or dms.BasicType.
+    for(String key: this.basicTypeList){
+      if(this.completeInheritanceRelations.get(key) != null){
+        for(String type : this.completeInheritanceRelations.get(key)){
+          if(!this.basicTypeList.contains(type)){
+            //the type is not a basic type or not inheriting from basic type.
+            System.err.println(key + " inherits from a non-basic type: " + type);
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+  
+  public boolean checkBasicTypeWellformedness_CyclicInheritance(){
+    //There is no circularity in the basic type sub-type hierarchy.
+    for(String key: this.basicTypeList){
+      if(this.completeInheritanceRelations.get(key) != null){
+        ArrayList<String> fullList = this.completeInheritanceRelations.get(key);
+        if(fullList.contains(key)){
+          //If the complete inheritance list contains the root type, it is cyclic.
+          System.err.println(key + " is cyclic: " + fullList);
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+  
+  
+  private void buildInheritanceHierarchy(String original, String current, ArrayList<String> fullList){
+    ArrayList<String> hierarchyList = this.inheritanceRelations.get(current);
+    if(hierarchyList == null || hierarchyList.isEmpty()){
+      return;
+    } else{
+      for(String parent: hierarchyList){
+        fullList.add(parent);
+        
+        //Cyclic inheritance detected.
+        //We won't notice anything here but stop building the hierarchy.
+        if(parent.equals(original)) return;
+        
+        buildInheritanceHierarchy(original, parent, fullList);
+      }
+    }
+  }
+}
