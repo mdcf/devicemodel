af70c6d1bd97d6a4677e868c4af239e9eab325d7
Sam Procter <samprocter@k-state.edu>
2013-06-11 12:18:13 -0500

    Added the second version of the NellcorPulseOx.

---
+++ mdcf-dms-example/src/main/scala/edu/ksu/cis/santos/mdcf/dms/examplev2/device/NellcorPulseOx.scala
@@ -0,0 +1,153 @@
+/*
+Copyright (c) 2013 Robby, John Hatcliff, Kansas State University.        
+All rights reserved. This program and the accompanying materials      
+are made available under the terms of the Eclipse Public License v1.0 
+which accompanies this distribution, and is available at              
+http://www.eclipse.org/legal/epl-v10.html                             
+*/
+
+package edu.ksu.cis.santos.mdcf.dms.examplev2.device
+
+import edu.ksu.cis.santos.mdcf.dms._
+import edu.ksu.cis.santos.mdcf.dms.examplev2._
+import edu.ksu.cis.santos.mdcf.dms.examplev2.schema._
+import edu.ksu.cis.santos.mdcf.dms.examplev2.clas._
+
+class NellcorGetExchange extends ICE_Get_Exchange {
+  override val access : Option[ICE_Security_Access_Read] = None
+  override val separation : NatRange = new NatRange {
+    override val min : Nat = 10
+    override val max : Nat = 50
+  }
+  override val serviceTime : NatRange = new NatRange {
+    override val min : Nat = 0
+    override val max : Nat = 50
+  }
+}
+
+class NellcorSetExchange extends ICE_Set_Exchange{
+  override val access : Option[ICE_Security_Access_Write] = None
+  override val separation : NatRange = new NatRange {
+    override val min : Nat = 10
+    override val max : Nat = 50
+  }
+  override val serviceTime : NatRange = new NatRange {
+    override val min : Nat = 0
+    override val max : Nat = 50
+  }
+}
+
+class NellcorPeriodicExchange extends ICE_Periodic_Exchange {
+  override val access : Option[ICE_Security_Access_Read] = None
+  override val separation : NatRange = new NatRange {
+    override val min : Nat = 98
+    override val max : Nat = 102
+  }
+  override val rate : NatRange = new NatRange {
+    override val min : Nat = 50
+    override val max : Nat = 150
+  }
+}
+
+class NellcorSporadicExchange extends ICE_Sporadic_Exchange {
+  override val access : Option[ICE_Security_Access_Read] = None
+  override val separation : NatRange = new NatRange {
+    override val min : Nat = 98
+    override val max : Nat = 102
+  }
+}
+
+final class NellcorPulseOx extends ICE_MDS {
+  override val IEEE11073_MDC_ATTR_SYS_TYPE : IEEE11073_TYPE = "Nellcor PO"
+
+  override val manufacturerModel : ICE_ManufacturerModel = new ICE_ManufacturerModel {
+    override val MDC_ATTR_ID_MODEL : IEEE11073_SystemModel = new IEEE11073_SystemModel {
+      override val manufacturer : String = "Nellcor"
+      override val model_number : String = "Nellcor 2000"
+    }
+    override val credentials : Map[String, ICE_Security_Certificate] = Map()
+  }
+
+  override val vmds : Map[String, ICE_VMD] = Map(
+    "pulseox" -> new ICE_PulseOx_VMD {
+      override val status : ICE_VMD_Status = new ICE_VMD_Status {
+        override val state : ICE_VMD_State = ICE_VMD_StateValue.Ok
+        override val access : Option[ICE_Security_Access_Read] = None
+        override val exchange : ICE_Get_Exchange = new NellcorGetExchange {}
+      }
+
+      override val channels : Map[String, ICE_Channel] = Map(
+        "spo2" -> new ICE_SpO2_Channel {
+          override val MDC_ATTR_ID_PARAM_GRP : IEEE11073_OID_TYPE = "SpO2 Channel"
+          override val alerts : Map[String, ICE_Alert] = Map()
+          override val metrics : Map[String, ICE_Metric] = Map(
+            "spo2_num" -> new ICE_SpO2_Numeric {
+              override val range : FloatRange = new FloatRange {
+                override val min : Float = 0.0
+                override val max : Float = 100.0
+              }
+              override val exchanges : Map[String, ICE_Data_Exchange] = Map(
+                "get" -> new NellcorGetExchange {},
+                "periodic" -> new NellcorPeriodicExchange {}
+              )
+              override val alerts : Map[String, ICE_Alert] = Map(
+                "spo2rangealert" -> new ICE_FloatRangeValueAlert{
+                  override val MDC_ATTR_ID_PHYSIO: IEEE11073_OID_TYPE = "PLACEHOLDER ID"
+                  override val MDC_ATTR_UNIT_CODE: IEEE11073_OID_TYPE = "PLACEHOLDER UNIT CODE"
+                  override val get = new NellcorGetExchange {}
+                  override val set = Some(new NellcorSetExchange {})
+                  override val access = None
+                  override val exchange = new NellcorSporadicExchange {}
+                  override val set_payload = None
+                  override val get_payload = new ICE_FloatRangeSettingGetPayload {
+                    override val payload = new ICE_FloatRangeSettingPayloadContent {
+                      override val min : Float = 0.0
+                      override val max : Float = 100.0
+                    }
+                  }
+                }
+              )
+            })
+          override val settings : Map[String, ICE_Setting] = Map()
+          override val statuses : Map[String, ICE_Status] = Map()
+          override val actions : Map[String, ICE_Action] = Map()
+        },
+        "pulserate" -> new ICE_SpO2_Channel {
+          override val MDC_ATTR_ID_PARAM_GRP : IEEE11073_OID_TYPE = "Pulse Rate Channel"
+          override val alerts : Map[String, ICE_Alert] = Map()
+          override val metrics : Map[String, ICE_Metric] = Map(
+            "pulserate_num" -> new ICE_PulseRate_Numeric {
+              override val range : FloatRange = new FloatRange {
+                override val min : Float = 20.0
+                override val max : Float = 300.0
+              }
+              override val exchanges : Map[String, ICE_Data_Exchange] = Map(
+                "get" -> new NellcorGetExchange {},
+                "periodic" -> new NellcorPeriodicExchange {}
+              )
+              override val alerts : Map[String, ICE_Alert] = Map(
+                "pulseraterangealert" -> new ICE_FloatRangeValueAlert{
+                  override val MDC_ATTR_ID_PHYSIO: IEEE11073_OID_TYPE = "PLACEHOLDER ID"
+                  override val MDC_ATTR_UNIT_CODE: IEEE11073_OID_TYPE = "PLACEHOLDER UNIT CODE"
+                  override val get = new NellcorGetExchange {}
+                  override val set = Some(new NellcorSetExchange {})
+                  override val access = None
+                  override val exchange = new NellcorSporadicExchange {}
+                  override val set_payload = None
+                  override val get_payload = new ICE_FloatRangeSettingGetPayload {
+                    override val payload = new ICE_FloatRangeSettingPayloadContent {
+                      override val min : Float = 40.0
+                      override val max : Float = 140.0
+                    }
+                  }
+                })
+            }
+          )
+          override val settings : Map[String, ICE_Setting] = Map()
+          override val statuses : Map[String, ICE_Status] = Map()
+          override val actions : Map[String, ICE_Action] = Map()
+        }
+      )
+    }
+  )
+}
