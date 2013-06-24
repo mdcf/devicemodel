5f184408f40c0967911e31a364e26f8b6a616266
Robby <robby@k-state.edu>
2013-06-24 11:10:26 -0500

    Removed examplev3 that was mistakenly committed by Yu Jin Kim.

--- mdcf-dms-example/src/main/scala/edu/ksu/cis/santos/mdcf/dms/examplev3/package.scala
+++
@@ -1,7 +0,0 @@
-package edu.ksu.cis.santos.mdcf.dms
-
-import edu.ksu.cis.santos.mdcf.dms.examplev3.String
-package object examplev3 {
-  import language.implicitConversions
-  implicit def sp[T](p : (java.lang.String, T)) = (String(p._1), p._2)
-}
