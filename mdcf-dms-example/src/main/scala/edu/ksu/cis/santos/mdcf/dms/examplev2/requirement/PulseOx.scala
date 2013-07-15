a062a22faf51c852c5e00b807d3ed93f57f693ac
Sam Procter <samprocter@k-state.edu>
2013-07-15 15:35:39 -0500

    Updated the PulseOx display app requirements to the new style.

--- mdcf-dms-example/src/main/scala/edu/ksu/cis/santos/mdcf/dms/examplev2/requirement/PulseOx.scala
+++ mdcf-dms-example/src/main/scala/edu/ksu/cis/santos/mdcf/dms/examplev2/requirement/PulseOx.scala
@@ -7,41 +7,42 @@ import edu.ksu.cis.santos.mdcf.dms.examplev2.clas.ICE_PulseRate_Numeric
 import edu.ksu.cis.santos.mdcf.dms.examplev2.schema.ICE_Periodic_Exchange
 
 trait PulseOxDisplayApp {
-  val dev : ICE_VMD
+  val spo2 : ICE_SpO2_Numeric
+  val pulserate : ICE_PulseRate_Numeric
+  val spo2_ex : ICE_Periodic_Exchange
+  val pr_ex : ICE_Periodic_Exchange
 }
 
 object PulseOxDisplayApp {
   @Inv
   val reqSpO2 : Predicate[PulseOxDisplayApp] =
-    pred { vmd : PulseOxDisplayApp =>
-      vmd.dev.channels.values.exists(
-        _.metrics.values.exists(
-          _ match {
-            case spo2num : ICE_SpO2_Numeric =>
-              spo2num.exchanges.values.exists(
-                _ match {
-                  case spo2Exchange : ICE_Periodic_Exchange => true
-                  case _                                    => false
-                })
-            case _ =>
-              false
-          }))
+    pred { apprec : PulseOxDisplayApp =>
+      apprec.spo2 match {
+        case spo2 : ICE_SpO2_Numeric =>
+          spo2.exchanges.values.exists(
+            _ match {
+              case exch : ICE_Periodic_Exchange =>
+                apprec.spo2_ex == exch
+              case _ => false
+            }
+          )
+        case _ => false
+      }
     }
 
   @Inv
   val reqPulseRate : Predicate[PulseOxDisplayApp] =
-    pred { vmd : PulseOxDisplayApp =>
-      vmd.dev.channels.values.exists(
-        _.metrics.values.exists(
-          _ match {
-            case prnum : ICE_PulseRate_Numeric =>
-              prnum.exchanges.values.exists(
-                _ match {
-                  case prExchange : ICE_Periodic_Exchange => true
-                  case _                                  => false
-                })
-            case _ =>
-              false
-          }))
+    pred { apprec : PulseOxDisplayApp =>
+      apprec.pulserate match {
+        case pulserate : ICE_PulseRate_Numeric =>
+          pulserate.exchanges.values.exists(
+            _ match {
+              case exch : ICE_Periodic_Exchange =>
+                apprec.pr_ex == exch
+              case _ => false
+            }
+          )
+        case _ => false
+      }
     }
 }
